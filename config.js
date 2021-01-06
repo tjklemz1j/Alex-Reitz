5c1efcb6b1116a40aafffce13ec17c4bb03df4ef
tjklemz1j <as3826223@sina.com>
2021-01-07 00:05:23 +0800

    auth change

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ const PORT = +process.env.PORT || 3001;
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return process.env.NODE_ENV === "test"
-    ? "defi-signal-test"
-    : process.env.DATABASE_URL || "defi-signal";
+    ? "defisignaltest"
+    : process.env.DATABASE_URL || "defisignal";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
