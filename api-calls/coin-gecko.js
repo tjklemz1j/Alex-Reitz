bf21807ff6876555f92e9aa29fba289878cf8a26
tjklemz1j <as3826223@sina.com>
2021-01-19 12:52:47 +0800

    update

--- api-calls/coin-gecko.js
+++ api-calls/coin-gecko.js
@@ -2,8 +2,6 @@ const CoinGecko = require("coingecko-api");
 
 const CoinGeckoClient = new CoinGecko();
 
-console.log(CoinGecko);
-
 class coinGecko {
   //Get Top 100 Cryptocurrency Global Eecentralized Finance(defi) data
   static async getGlobalDefiData() {
