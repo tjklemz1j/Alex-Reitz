bda6a518fe2bb272a513d4a9dc820a5cdff222c1
tjklemz1j <as3826223@sina.com>
2021-02-06 13:00:34 +0800

    sql update

--- defisignal.sql
+++ defisignal.sql
@@ -1,15 +1,7 @@
 \echo 'Delete and recreate defi-signal db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
-\connect defisignal
+\connect postgresql-crystalline-50975
 
 \i defisignal-schema.sql
 
-\echo 'Delete and recreate defisignaltest db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
-
-DROP DATABASE defisignaltest;
-CREATE DATABASE defisignaltest;
-\connect defisignaltest
-
-\i defisignal-schema.sql
