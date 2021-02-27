11f1568f0be453f3f2869437042ce0167b6107fa
tjklemz1j <as3826223@sina.com>
2021-02-28 05:29:30 +0800

    update

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const morgan = require("morgan");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
